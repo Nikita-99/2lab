439c79eb30e87af3b06b48295ce12bd7802d4202
Nikita-99 <48046056+Nikita-99@users.noreply.github.com>
2019-03-06 10:11:15 +0300

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,7 @@
+print("Введите число, которое будет меньше заданного")
+my_number = 10
+d=int(input("user_nuber="))
+while d > my_number:
+    print("Неверно")
+    d=int(input("Введите новое число"))
+print("user_number меньше my_nuber")
---
+++ 2.4.py
@@ -0,0 +1,2 @@
+str=input('Строка:')
+print(''.join(s for s in str if s.isdigit()))
